9556cafeb03af8b50491b90ee7f26a4a55a1fa1f
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-03-13 21:34:31 +0530

    FEAT: using jsonplaceholder data

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Todo} from '../../models/Todos';
+import {TodoService} from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -9,28 +10,12 @@ import {Todo} from '../../models/Todos';
 export class TodosComponent implements OnInit {
     todos:Todo[];
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
-      this.todos = [
-          {
-              id: 1,
-              title: 'Todo One',
-              completed: false
-          },
-
-          {
-              id: 2,
-              title: 'Todo Two',
-              completed:true 
-          },
-
-          {
-              id: 1,
-              title: 'Todo Three',
-              completed: false
-          }
-      ]
+      this.todoService.getTodos().subscribe(todos => {
+          this.todos = todos;
+      });
   }
 
 }
